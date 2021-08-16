0d2110135a981889a0f5912f5fa624d812d6c1b1
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-08-16 17:29:37 +0530

    Created Second Component using CLI command "ng g c ComponentName"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './FirstComponent/FirstComponent.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/second-component/second-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-component',
+  templateUrl: './second-component.component.html',
+})
+export class SecondComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
