817eb7c965715b45aac497595ddcfb13dceab90c
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-08-16 16:39:55 +0530

    Made first Component

---
+++ src/app/FirstComponent/FirstComponent.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-FirstComponent',
+    templateUrl: './FirstComponent.component.html'
+})
+export class FirstComponent {
+
+}
