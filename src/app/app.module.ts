bb17aab322b5a7eb4745864843442bc6ca157cf2
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-08-16 16:47:11 +0530

    Registered our FirstComponent in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './FirstComponent/FirstComponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
