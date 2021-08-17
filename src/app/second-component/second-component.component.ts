8af8728e79862e3cf21bc19397d780420e4297ca
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-08-17 16:35:45 +0530

    using inline template in second component

--- src/app/second-component/second-component.component.ts
+++ src/app/second-component/second-component.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-second-component',
-  templateUrl: './second-component.component.html',
+  template: `
+    <app-FirstComponent></app-FirstComponent>
+    <app-FirstComponent></app-FirstComponent>`,
 })
 export class SecondComponentComponent implements OnInit {
 
